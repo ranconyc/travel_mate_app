cd021475ceae118ee43fc96d2a5a142f3d96426c
Matt Gordon <mattg1520@gmail.com>
2020-05-26 14:04:20 +0300

    changed back to next destination

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-  ACTIVITIES = ['Night Life', 'Beach', 'Nature', 'Food', 'Site Seeing', 'Sport', 'Classes', 'Next Stop', 'Culture', 'Music']
+  ACTIVITIES = ['Night Life', 'Beach', 'Nature', 'Food', 'Site Seeing', 'Sport', 'Classes', 'Next Destination', 'Culture', 'Music']
   VIBES = ['Chill', 'Pumped', 'Exploring', 'Fun', 'Heavy', 'Good Vibes Only']
   belongs_to :user
   has_many :users, through: :members
