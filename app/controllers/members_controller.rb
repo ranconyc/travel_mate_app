0bb7383402e4e9eeb5b302501941c120103122ee
Gil Grin <gilgi@mta.ac.il>
2020-05-28 13:20:26 +0300

    added link to shoe page from activity card

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,18 @@
 class MembersController < ApplicationController
+  def create
+    @member = Member.new(member_params)
+    @member.user = current_user
+    @activity = @member.activity
+    if @member.save
+      redirect_to activity_path(@activity), notice: 'Member was successfully created.'
+    else
+      render '/'
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:activity)
+  end
 end
