0be677e14c3a7842baf9b2d2a042a26adcec7c73
Gil Grin <gilgi@mta.ac.il>
2020-06-01 14:12:59 +0300

    added search by nearby location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_074733) do
+ActiveRecord::Schema.define(version: 2020_06_01_081046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 2020_06_01_074733) do
     t.text "about"
     t.date "date_of_birth"
     t.string "hometown"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
