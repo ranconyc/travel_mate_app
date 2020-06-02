e04a4ee6700481bb59c3a6959bafbac489054518
Gil Grin <gilgi@mta.ac.il>
2020-06-02 15:41:57 +0300

    fixed activities chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,11 +1,23 @@
 class ChatsController < ApplicationController
   def index
-    # @activities = []
-    @activities = current_user.activities.order(:title)
+    @my_activities = current_user.activities.order(:title)
+    @my_joined_activities = getJoinedActivities
+    @activities = @my_activities + @my_joined_activities
   end
 
   def show
     @activity = Activity.find(params[:id])
     @message = Message.new
+  end
+
+  private
+
+  def getJoinedActivities
+    my_joined_activities_arr = []
+    members_arr = Member.where(user_id: current_user.id)
+    members_arr.each do |member|
+      my_joined_activities_arr << Activity.find(member.activity_id)
+    end
+    my_joined_activities_arr
   end
 end
